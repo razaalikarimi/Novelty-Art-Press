72158c6f056856b13c360950f5964920f90c7c26
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2025-12-09 11:55:54 +0530

    Update ProductsPage.js

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
