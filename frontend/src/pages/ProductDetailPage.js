690396d3364b66662a2737d1fa7b30a68d97a5a0
ALI RAZA KARIMI <123482649+razaalikarimi@users.noreply.github.com>
2025-12-09 11:55:35 +0530

    Update ProductDetailPage.js

--- frontend/src/pages/ProductDetailPage.js
+++ frontend/src/pages/ProductDetailPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 const ProductDetailPage = () => {
   const { id } = useParams();
